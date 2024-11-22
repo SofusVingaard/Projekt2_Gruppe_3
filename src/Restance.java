76fb9215ba11b79da39dfffa8a80c25548253678
MuniGlitch <chni0006@stud.kea.dk>
2024-11-22 11:20:32 +0100

    Linje nr

--- src/Restance.java
+++ src/Restance.java
@@ -1,12 +1,16 @@
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 
 public class Restance {
     private static final String FILNAVN = "src/Medlemmer.txt";
         public static void restance() throws Exception {
+
+            Scanner scanner = new Scanner(new File(FILNAVN));
             //læse alle linjer i vores fil
             List<String> linjer = Files.readAllLines(Path.of(FILNAVN));
 //nu skal jeg filtre alle de linjer som indeholder "Ikke betalt"
@@ -17,7 +21,13 @@ public class Restance {
             if (restance.isEmpty()) {
                 System.out.println("Fuld indtjening");
             } else {
-                System.out.println("Der er restance: " + restance);
+                // Vis de ledige tider på den ønskede dato
+                System.out.println("Restance på følgnede brugere ");
+                for (int i = 0; i < restance.size(); i++) {
+                    System.out.println((i + 1) + ": " + restance.get(i));  // Vis linje nummer og indhold
+
+                }
+
             }
         }
     public static void main(String[] args) throws Exception {
