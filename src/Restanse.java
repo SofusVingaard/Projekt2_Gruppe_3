4d50acc786bfb623ad5c79633e7b3f56cf5ebea5
Arfi <arma0001@stud.kea.dk>
2024-11-23 20:01:05 +0100

    Jeg har tilføjet SwimShop og Dailypass i en seperat klasse men kan nem inkulderes i medlemsreg og ligende

--- src/Restanse.java
+++ src/Restanse.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 
-public class Restance {
+public class Restanse {
     private static final String FILNAVN = "src/Medlemmer.txt";
         public static void restance() throws Exception {
 
@@ -18,11 +18,13 @@ public class Restance {
                     .filter(linje -> linje.contains("Ikke betalt"))
                     .collect(Collectors.toList());
             // Hvis der ikke er nogen "Ikke betalt"
+            //(Jeg prøvede at bruge null for at se om listen var tom for ikke betalt, men da collect(Collectors.toList()) altid returnere en liste ville den ikke compile)
+            //Så i stedet for null bruger jeg isEmpty for at tjekke om der nogen i restance
             if (restance.isEmpty()) {
                 System.out.println("Fuld indtjening");
             } else {
                 // Vis de ledige tider på den ønskede dato
-                System.out.println("Restance på følgnede brugere ");
+                System.out.println("Restance på følgende brugere ");
                 for (int i = 0; i < restance.size(); i++) {
                     System.out.println((i + 1) + ": " + restance.get(i));  // Vis linje nummer og indhold
 
---
+++ src/SwimShop.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+public class SwimShop {
+    public void startService() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Velkommen til Delfin");
+        System.out.println("Vælg en af de følgende");
+        System.out.println("1. Dailypass for Voksne (70 kr. ) ");
+        System.out.println("2. Dailypass for Børn (30 kr.)");
+        System.out.println("3. Dailypass for Senior/Studerende (50 kr. )");
+        System.out.println("4. Dailypass for Joe Rogan (1 kr. )");
+
+        int choice = scanner.nextInt();
+        double dailyPass = 0;
+
+        switch (choice) {
+            case 1:
+                dailyPass = 70;
+                break;
+            case 2:
+                dailyPass = 30;
+                break;
+            case 3:
+                dailyPass = 50;
+                break;
+            case 4:
+                dailyPass = 1;
+                break;
+            default:
+                System.out.println("Ugyldigt");
+                return;
+        }
+
+        System.out.println("Tilføj produkt til kurv");
+        System.out.println("1. Badetøj til Mænd (300 kr. )");
+        System.out.println("2. Badetøj til Kvinder (800 kr. )");
+        System.out.println("3. Badetøj til Børn (1000 kr. )");
+        System.out.println("4. Svømmebriller (400 kr. )");
+        System.out.println("5. Håndklæde (5 kr. )");
+        System.out.println("6. Tast 0 for ingen");
+
+        int addOn = scanner.nextInt();
+        double productCost = 0;
+
+        switch (addOn) {
+            case 1:
+                productCost = 300;
+                break;
+            case 2:
+                productCost = 800;
+                break;
+            case 3:
+                productCost = 1000;
+                break;
+            case 4:
+                productCost = 400;
+                break;
+            case 5:
+                productCost = 5;
+                break;
+            case 0:
+                productCost = 0;
+                break;
+            default:
+                System.out.println("Ugyldigt");
+        }
+
+        double totalCost = dailyPass + productCost;
+        System.out.println("Totalprisen " + totalCost + "kr.");
+        scanner.close();
+
+        }
+
+    public static void main(String[] args) {
+        SwimShop swimShop = new SwimShop();
+        swimShop.startService();
+
+
+
+        }
+    }
+
+
+
