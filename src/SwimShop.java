770ccc79abfc24f8b8d5f96f593ed4c113c076b8
Arfi <arma0001@stud.kea.dk>
2024-11-25 11:33:34 +0100

    Fejl i swimShop

--- src/SwimShop.java
+++ src/SwimShop.java
@@ -31,55 +31,53 @@ public class SwimShop {
                 System.out.println("Ugyldigt");
                 return;
         }
+        public void startService2() {
+            Scanner scanner2 = new Scanner(System.in);
+            System.out.println("Tilføj produkt til kurv");
+            System.out.println("1. Badetøj til Mænd (300 kr. )");
+            System.out.println("2. Badetøj til Kvinder (800 kr. )");
+            System.out.println("3. Badetøj til Børn (1000 kr. )");
+            System.out.println("4. Svømmebriller (400 kr. )");
+            System.out.println("5. Håndklæde (5 kr. )");
+            System.out.println("6. Tast 0 for ingen");
 
-        System.out.println("Tilføj produkt til kurv");
-        System.out.println("1. Badetøj til Mænd (300 kr. )");
-        System.out.println("2. Badetøj til Kvinder (800 kr. )");
-        System.out.println("3. Badetøj til Børn (1000 kr. )");
-        System.out.println("4. Svømmebriller (400 kr. )");
-        System.out.println("5. Håndklæde (5 kr. )");
-        System.out.println("6. Tast 0 for ingen");
+            int addOn = scanner.nextInt();
+            double productCost = 0;
 
-        int addOn = scanner.nextInt();
-        double productCost = 0;
+            switch (addOn) {
+                case 1:
+                    productCost = 300;
+                    break;
+                case 2:
+                    productCost = 800;
+                    break;
+                case 3:
+                    productCost = 1000;
+                    break;
+                case 4:
+                    productCost = 400;
+                    break;
+                case 5:
+                    productCost = 5;
+                    break;
+                case 0:
+                    productCost = 0;
+                    break;
+                default:
+                    System.out.println("Ugyldigt");
+            }
 
-        switch (addOn) {
-            case 1:
-                productCost = 300;
-                break;
-            case 2:
-                productCost = 800;
-                break;
-            case 3:
-                productCost = 1000;
-                break;
-            case 4:
-                productCost = 400;
-                break;
-            case 5:
-                productCost = 5;
-                break;
-            case 0:
-                productCost = 0;
-                break;
-            default:
-                System.out.println("Ugyldigt");
+            double totalCost = dailyPass + productCost;
+            System.out.println("Totalprisen " + totalCost + "kr.");
+            scanner.close();
+
         }
-
-        double totalCost = dailyPass + productCost;
-        System.out.println("Totalprisen " + totalCost + "kr.");
-        scanner.close();
-
-        }
-
-    public static void main(String[] args) {
-        SwimShop swimShop = new SwimShop();
-        swimShop.startService();
 
 
 
         }
     }
+
 
 
 
