3c60e39534bad8b0313b9bc1d2bc22959cd94b5b
Gustav <guwi0001@stud.kea.dk>
2024-11-25 11:13:47 +0100

    Skrevet i svømmeklub klassen

--- src/Svømmehold.java
+++ src/Svømmehold.java
@@ -1,13 +1,30 @@
+import java.util.Scanner;
+
 public class Svømmehold {
 
-    public static final String FILNAVN = "src/Medlemmer.txt";
+    private static final String FILNAVN = "srcMedlem,mer.txt";
+    boolean svømmehold = true;
 
-    //Efter man har trykket på vælg hold skal man kunne tilføje
-    public static void tilføjHold() {
+    public static void hold() {
+        Scanner keyboard = new Scanner(System.in);
+        String ALDER = keyboard.nextLine();
+
+        System.out.println("");
+
+        if (ALDER.equals()){
+            System.out.println("Du er på junior holdet");
+        }else if (ALDER.equals(18-59)){
+            System.out.println("Du er på Senior holdet");
+        }else {
+            System.out.println("Du er på 60+ holdet");
+        }
+
 
     }
 
 
 
+        }
     }
+
 
