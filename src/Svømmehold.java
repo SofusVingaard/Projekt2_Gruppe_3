5528b7c7b34683ca0a3a665ffc6b90fb5601ec63
Gustav <guwi0001@stud.kea.dk>
2024-11-26 01:10:22 +0100

    Fik rettet, så det burdes kunne bruges i main nu

--- src/Svømmehold.java
+++ src/Svømmehold.java
@@ -1,30 +1,38 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
 
-/* public class Svømmehold {
+public class Svømmehold {
 
-    private static final String FILNAVN = "srcMedlem,mer.txt";
-    boolean svømmehold = true;
+    private static final String FILNAVN = "srcMedlemmer.txt";
+    private static final String ALDER = "Alder: ";
 
-    public static void hold() {
-        Scanner keyboard = new Scanner(System.in);
-        String ALDER = keyboard.nextLine();
+    public static void main(String[] args) {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(FILNAVN));
 
-        System.out.println("");
+            for (String line : lines) {
+                if (line.startsWith(ALDER)) {
 
-        if (ALDER.equals()){
-            System.out.println("Du er på junior holdet");
-        }else if (ALDER.equals(18-59)){
-            System.out.println("Du er på Senior holdet");
-        }else {
-            System.out.println("Du er på 60+ holdet");
+                    int alder = Integer.parseInt(line.replace(ALDER, "").trim());
+                }
+                Scanner keyboard = new Scanner(System.in);
+                System.out.println("Indtast din alder");
+                int alder = keyboard.nextInt();
+
+                if (alder >= 1 && alder <= 17) {
+                    System.out.println("Du er på junior holdet");
+                } else if (alder >= 18 && alder <= 59) {
+                    System.out.println("Du er på senior holdet");
+                } else if (alder >= 60) {
+                } else {
+                    System.out.println("Ugyldig alder. Skriv igen");
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-
-
     }
-
-
-
-        }
-
-
-*/
+}
