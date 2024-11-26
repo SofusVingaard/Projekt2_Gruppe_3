cc2234b02410c2d07a6fbf30f8c4af1443c50803
Gustav <guwi0001@stud.kea.dk>
2024-11-26 11:29:49 +0100

    Vi har fået lavet flere detaljer på den

--- src/Svømmehold.java
+++ src/Svømmehold.java
@@ -9,8 +9,10 @@ public class Svømmehold {
     private static final String ALDER = "Alder: ";
 
     public static void main(String[] args) {
+        disciplin();
         try {
             List<String> lines = Files.readAllLines(Paths.get(FILNAVN));
+
 
             for (String line : lines) {
                 if (line.startsWith(ALDER)) {
@@ -34,4 +36,56 @@ public class Svømmehold {
             throw new RuntimeException(e);
         }
     }
+
+
+    public static void disciplin(){
+
+
+        boolean kg = true;
+        Scanner kb = new Scanner(System.in);
+        int hold;
+        while(kg)
+
+        {
+            System.out.println("Her er fem forskellige discipliner");
+            System.out.println("1. Bryst");
+            System.out.println("2. Crawl");
+            System.out.println("3. Butterfly");
+            System.out.println("4. Ryg");
+            System.out.println("0. Afslut");
+            hold = kb.nextInt();
+            kb.nextLine();
+            if (hold==0){
+                break;
+            } else if (hold>4){
+                kg=false;
+
+            }
+
+            switch (hold){
+                case 1:
+                    System.out.println("Du har valgt Bryst");
+                    System.out.println("Phillip Mortensen er din træner, der er hold tider Mandag");
+                    break;
+                case 2:
+                    System.out.println("Du har valgt Crawl");
+                    System.out.println("Phillip Mortensen er din træner, der er hold tider Tirsdag");
+                    break;
+                case 3:
+                    System.out.println("Du har valgt Butterfly");
+                    System.out.println("Mads Mortensen er din træner, der er hold tider Onsdag");
+                    break;
+                case 4:
+                    System.out.println("Du har valgt Ryg");
+                    System.out.println("Du har Mads Mortensen er din træner, der er hold tider Torsdag");
+
+                case 5:
+                    System.out.println("Afsluttet");
+                    break;
+
+            }
+        }
+    }
 }
+
+
