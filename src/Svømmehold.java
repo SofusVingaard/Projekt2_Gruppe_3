d1b87588faf5a6ad2996f00c6ffeacaeea1f053a
GusViking <guwi0001@stud.kea.dk>
2024-11-27 11:16:25 +0100

    Rettet de mange fejl

--- src/Svømmehold.java
+++ src/Svømmehold.java
@@ -1,30 +1,60 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
 
-/* public class Svømmehold {
 
+public class Svømmehold {
     private static final String FILNAVN = "srcMedlem,mer.txt";
-    boolean svømmehold = true;
+    private static final String ALDER = "Alder: ";
 
-    public static void hold() {
-        Scanner keyboard = new Scanner(System.in);
-        String ALDER = keyboard.nextLine();
-
-        System.out.println("");
-
-        if (ALDER.equals()){
-            System.out.println("Du er på junior holdet");
-        }else if (ALDER.equals(18-59)){
-            System.out.println("Du er på Senior holdet");
-        }else {
-            System.out.println("Du er på 60+ holdet");
-        }
-
+    public static void main(String[] args) {
+        disciplin();
 
     }
 
+                            public static void disciplin() {
+                                boolean kg = true;
+                                Scanner kb = new Scanner(System.in);
+                                int hold;
+                                while (kg) {
+                                    System.out.println("Her er fem forskellige discipliner");
+                                    System.out.println("1. Bryst");
+                                    System.out.println("2. Crawl");
+                                    System.out.println("3. Butterfly");
+                                    System.out.println("4. Ryg");
+                                    System.out.println("0. Afslut");
+                                    hold = kb.nextInt();
+                                    kb.nextLine();
+                                    if (hold == 0) {
+                                        break;
+                                    } else if (hold > 4) {
+                                        kg = false;
 
+                                    }
 
-        }
-
-
-*/
+                                    switch (hold) {
+                                        case 1:
+                                            System.out.println("Du har valgt Bryst");
+                                            System.out.println("Phillip Mortensen er din træner, der er hold tider Mandag");
+                                            break;
+                                        case 2:
+                                            System.out.println("Du har valgt Crawl");
+                                            System.out.println("Phillip Mortensen er din træner, der er hold tider Tirsdag");
+                                            break;
+                                        case 3:
+                                            System.out.println("Du har valgt Butterfly");
+                                            System.out.println("Mads Mortensen er din træner, der er hold tider Onsdag");
+                                            break;
+                                        case 4:
+                                            System.out.println("Du har valgt Ryg");
+                                            System.out.println("Du har Mads Mortensen er din træner, der er hold tider Torsdag");
+                                            break;
+                                        case 5:
+                                            System.out.println("Afsluttet");
+                                            break;
+                                    }
+                                }
+                            }
+            }
