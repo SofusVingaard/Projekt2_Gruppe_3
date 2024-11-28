2080c434d35f6b6b70814f40f5cdbff4c06c6678
Gustav <guwi0001@stud.kea.dk>
2024-11-28 10:28:59 +0100

    Tolføjet distancer til hver disciplin

--- src/Svømmehold.java
+++ src/Svømmehold.java
@@ -16,43 +16,78 @@ public class Svømmehold {
         int hold;
         while (kg) {
             System.out.println("Her er fem forskellige discipliner");
-            System.out.println("1. Bryst");
-            System.out.println("2. Crawl");
-            System.out.println("3. Butterfly");
-            System.out.println("4. Ryg");
+            System.out.println("1. Bryst - 100 m");
+            System.out.println("2. Bryst - 200 m");
+            System.out.println("3. Bryst - 400 m");
+            System.out.println("4. Crawl - 100 m");
+            System.out.println("5. Crawl - 200 m");
+            System.out.println("6. Crawl - 400 m");
+            System.out.println("7. Butterfly - 100 m");
+            System.out.println("8. Butterfly - 200 m");
+            System.out.println("9. Butterfly - 400 m");
+            System.out.println("10. Ryg - 100 m");
+            System.out.println("11. Ryg - 200 m");
+            System.out.println("12. Ryg - 400 m");
             System.out.println("0. Afslut");
             hold = kb.nextInt();
             kb.nextLine();
             if (hold == 0) {
                 System.out.println("Afslutter programmet");
                 break;
-            } else if (hold > 4 || hold < 0) {
+            } else if (hold > 12 || hold < 0) {
                 System.out.println("Ugyldigt valg, proev igen");
                 continue;
 
             }
-
             switch (hold) {
                 case 1:
-                    System.out.println("Du har valgt Bryst");
+                    System.out.println("Du har valgt Bryst - 100 m");
                     System.out.println("Phillip Mortensen er din træner, der er hold tider Mandag");
                     break;
                 case 2:
-                    System.out.println("Du har valgt Crawl");
-                    System.out.println("Phillip Mortensen er din træner, der er hold tider Tirsdag");
+                    System.out.println("Du har valgt Bryst - 200 m");
+                    System.out.println("Phillip Mortensen er din træner, der er hold tider Mandag");
                     break;
                 case 3:
-                    System.out.println("Du har valgt Butterfly");
-                    System.out.println("Mads Mortensen er din træner, der er hold tider Onsdag");
+                    System.out.println("Du har valgt Bryst - 400 m");
+                    System.out.println("Phillip Mortensen er din træner, der er hold tider Mandag");
                     break;
                 case 4:
-                    System.out.println("Du har valgt Ryg");
-                    System.out.println("Du har Mads Mortensen er din træner, der er hold tider Torsdag");
+                    System.out.println("Du har valgt Crawl - 100 m");
+                    System.out.println("Phillip Mortensen er din træner, der er hold tider Tirsdag");
                     break;
                 case 5:
-                    System.out.println("Afsluttet");
+                    System.out.println("Du har valgt Crawl - 200 m");
+                    System.out.println("Phillip Mortensen er din træner, der er hold tider Tirsdag");
                     break;
-
+                case 6:
+                    System.out.println("Du har valgt Crawl - 400 m");
+                    System.out.println("Phillip Mortensen er din træner, der er hold tider Tirsdag");
+                    break;
+                case 7:
+                    System.out.println("Du har valgt Butterfly - 100 m");
+                    System.out.println("Mads Mortensen er din træner, der er hold tider Onsdag");
+                    break;
+                case 8:
+                    System.out.println("Du har valgt Butterfly - 200 m");
+                    System.out.println("Mads Mortensen er din træner, der er hold tider Onsdag");
+                    break;
+                case 9:
+                    System.out.println("Du har valgt Butterfly - 400 m");
+                    System.out.println("Mads Mortensen er din træner, der er hold tider Onsdag");
+                    break;
+                case 10:
+                    System.out.println("Du har valgt Ryg - 100 m");
+                    System.out.println("Du har Mads Mortensen er din træner, der er hold tider Torsdag");
+                    break;
+                case 11:
+                    System.out.println("Du har valgt Ryg - 200 m");
+                    System.out.println("Du har Mads Mortensen er din træner, der er hold tider Torsdag");
+                    break;
+                case 12:
+                    System.out.println("Du har valgt Ryg - 400 m");
+                    System.out.println("Du har Mads Mortensen er din træner, der er hold tider Torsdag");
+                    break;
             }
         }
     }
