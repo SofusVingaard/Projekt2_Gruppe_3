ba17dfa1c1becf8c05c6f7a80cf69c5b962d494a
SofusVingaard <sovi0003@stud.kea.dk>
2024-12-11 09:01:19 +0100

    opdateret main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        //MåløvDolphins.main(args);
         System.out.println("Velkommen til Svømmeklubbben Målev Dolphins");
 
         Scanner keyboard = new Scanner(System.in);
@@ -11,27 +11,29 @@ public class Main {
         int tast;
 
 
-            while (keepGoing) {
+        while (keepGoing) {
             System.out.println("1: Se åbningstider:");
             System.out.println("2: Opret medlem:");
             System.out.println("3: Se medlemmer:");
             System.out.println("4: Gå til leaderboard:");
             System.out.println("5: Opret træningstid");
             System.out.println("6: Se om medlemmer er i restanse");
-            System.out.println("7: Køb vare i SwipShop");
-            System.out.println("8: Indtast et konkurrence stævne");
+            System.out.println("7: Køb dailyPass til svømmehallen");
+            System.out.println("8: Køb vare i SwipShop");
+            System.out.println("9: Indtast et konkurrence stævne");
+            System.out.println("10: Hvis du vil søge efter et konkurrence stævne");
             System.out.println("0: Luk Programmet:");
 
-                try { tast = keyboard.nextInt();
+            try { tast = keyboard.nextInt();
                 //clear vores scanner buffer
                 keyboard.nextLine();
 
-                 if (tast == 0) {
+                if (tast == 0) {
                     System.out.println("Tak for i dag");
                     keepGoing = false;
 
                 }
-                 else if (tast >= 10 || tast<=-1) {
+                else if (tast >= 11 || tast<=-1) {
                     System.out.println("ugyldigt");
                     return;
                 }
@@ -41,6 +43,8 @@ public class Main {
                         System.out.println("Vores åbningstider er");
                         System.out.println("Mandag-fredag 10-20");
                         System.out.println("lørdag-søndag 10-18");
+                        System.out.println();
+
                         break;
                     case 2:
                         MedlemsRegistering.tilføjMedlem();
@@ -60,18 +64,22 @@ public class Main {
                     case 7:
                         SwimShop.startService();
                         break;
-                    case 8:
+                    case 9:
                         MedlemsRegistering.konkurrenceStævne();
                         break;
-                 }
-            }
-                catch (InputMismatchException e) {
-                    System.out.println("Forkert input. Indtast et tal mellem 0 og 9");
-                    System.out.println();
-                    keyboard.nextLine();
-            } catch (Exception e){
-                    System.out.println("Der skete en fejl "+e.getMessage()+" prøv igen");
+                    case 10:
+                        MedlemsRegistering.visKonkurrencer();
+                        break;
                 }
+            }
+            catch (InputMismatchException e) {
+                System.out.println("Forkert input. Indtast et tal mellem 0 og 10");
+                System.out.println();
+                keyboard.nextLine();
+            } catch (Exception e){
+                System.out.println("Der skete en fejl "+e.getMessage()+" prøv igen");
+            }
+
         }
     }
 }
